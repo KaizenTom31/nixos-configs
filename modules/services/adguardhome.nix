3a3ed41655c7450416761d2bdfbf84a55b501446
Tom <77831931+KaizenTom31@users.noreply.github.com>
2025-10-13 19:44:29 +0000

    add adguard user to acme group to access acme certs for doh, dot, doq

--- modules/services/adguardhome.nix
+++ modules/services/adguardhome.nix
@@ -4,6 +4,29 @@
     services.adguardhome = {
       enable = true;
   };
-  networking.firewall.allowedUDPPorts = [ 53 ];
-  networking.firewall.allowedTCPPorts = [ 53 ];
+  
+  users.users.adguardhome = {
+    isSystemUser = true;
+    extraGroups = ["acme"];
+    group = "adguardhome";
+  };
+  users.groups.adguardhome = {};
+
+  networking.firewall.allowedUDPPorts = [ 53 853 ];
+  networking.firewall.allowedTCPPorts = [ 53 853 ];
+
+  services.nginx = {
+    virtualHosts = {
+      "dns.sh.tomwissing.de" = {
+        forceSSL = true;
+        quic = true;
+        http3 = true;
+        useACMEHost = "sh.tomwissing.de";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8000";
+          proxyWebsockets = true; # allows live updates if needed
+        };
+      };
+    };
+  };
 }
